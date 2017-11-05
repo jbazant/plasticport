f663e5d180812edebe33f2cf7af6a9559a3e12dd
Jiří Bažant <jiri.bazant@symbiodigital.com>
2017-11-05 22:31:32 +0100

    secure update

--- sites/default/themes/pp_blue/templates/html.tpl.php
+++ sites/default/themes/pp_blue/templates/html.tpl.php
@@ -48,6 +48,7 @@
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="default-src https:">
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
