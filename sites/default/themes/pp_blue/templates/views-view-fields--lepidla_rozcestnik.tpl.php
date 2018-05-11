478a78ddecde04d51c3c76c3bdf9a3fc8bdaf7de
Jiří Bažant <jiri.bazant@symbiodigital.com>
2018-05-11 20:02:48 +0200

    lepidla_rozcestnik template update

--- sites/default/themes/pp_blue/templates/views-view-fields--lepidla_rozcestnik.tpl.php
+++ sites/default/themes/pp_blue/templates/views-view-fields--lepidla_rozcestnik.tpl.php
@@ -4,12 +4,14 @@
  * User: jiri.bazant
  * Date: 10.12.17
  * Time: 23:07
+ *
+ * @var array $fields
  */
 ?>
 <div class="l-list__col">
     <div class="product-card__img"><?php print $fields['field_image']->content; ?></div>
-    <div class="product-card__content">
+    <a href="<?php print $fields['path']->content; ?>" class="product-card__content">
         <h3 class="heading--primary product-card__heading"><?php print $fields['field_short_title']->content; ?></h3>
         <p><?php print $fields['field_short']->content; ?></p>
-    </div>
+    </a>
 </div>
