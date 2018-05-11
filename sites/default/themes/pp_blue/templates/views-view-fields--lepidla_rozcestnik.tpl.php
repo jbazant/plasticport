5b78decf2917510d1d803f77650b6cd184ed250e
Jiří Bažant <jiri.bazant@symbiodigital.com>
2018-05-11 20:25:29 +0200

    graphical fixes - list of products

--- sites/default/themes/pp_blue/templates/views-view-fields--lepidla_rozcestnik.tpl.php
+++ sites/default/themes/pp_blue/templates/views-view-fields--lepidla_rozcestnik.tpl.php
@@ -9,9 +9,11 @@
  */
 ?>
 <div class="l-list__col">
-    <div class="product-card__img"><?php print $fields['field_image']->content; ?></div>
-    <a href="<?php print $fields['path']->content; ?>" class="product-card__content">
-        <h3 class="heading--primary product-card__heading"><?php print $fields['field_short_title']->content; ?></h3>
-        <p><?php print $fields['field_short']->content; ?></p>
+    <a href="<?php print $fields['path']->content; ?>" class="product-card">
+        <div class="product-card__img"><?php print $fields['field_image']->content; ?></div>
+        <div class="product-card__content">
+            <h3 class="heading--primary product-card__heading"><?php print $fields['field_short_title']->content; ?></h3>
+            <p><?php print $fields['field_short']->content; ?></p>
+        </div>
     </a>
 </div>
