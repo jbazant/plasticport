bfd03566af8a5359d0090dd89e317967b7fcc4fe
Jiri Bazant <jiri.bazant@memsource.com>
2021-02-07 19:29:39 +0100

    new homepage

--- sites/default/themes/pp_blue/templates/page--front.tpl.php
+++ sites/default/themes/pp_blue/templates/page--front.tpl.php
@@ -2,7 +2,7 @@
 /**
  * Created by PhpStorm.
  * User: jiri.bazant
- * Date: 07.06.16
+ * Date: 27.12.20
  * Time: 23:20
  */
 $theme_path = drupal_get_path('theme', 'pp_blue');
@@ -13,47 +13,6 @@ include($theme_path . '/templates/inc/header.php');
 <main id="content">
   <h1 class="heading--hp">Brána do světa plastů</h1>
 
-  <?php if ($tabs): ?><?php print render($tabs); ?><?php endif; ?>
-
-  <?php if ($page['featured']): ?>
-    <div id="featured">
-      <div class="section clearfix">
-        <?php print render($page['featured']); ?>
-      </div>
-    </div>
-  <?php endif; ?>
-
-  <section class="l-home-nav">
-    <a href="<?= url('/zakazkova-vyroba') ?>" class="l-home-nav__item">
-      <div class="l-home-nav__img">
-        <picture>
-          <source media="(min-width: 700px)"
-                  srcset="<?= url($theme_path . '/img/img-hp-01.jpg') . ', ' . url($theme_path . '/img/img-hp-01-2x.jpg') . ' 2x' ?>">
-          <?= theme('image', array(
-            'path' => $theme_path . '/img/img-hp-mobile-01.jpg',
-            'alt' => "Produkty vizuál",
-            'attributes' => array('srcset' => $directory . '/img/img-hp-01.jpg 2x'),
-          )) ?>
-        </picture>
-      </div>
-      <h2 class="l-home-nav__heading"><span>Zakázková výroba</span></h2>
-    </a>
-    <a href="<?= url('/produkty') ?>" class="l-home-nav__item">
-      <div class="l-home-nav__img">
-        <picture>
-          <source media="(min-width: 700px)"
-                  srcset="<?= url($theme_path . '/img/img-hp-02.jpg') . ', ' . url($theme_path . '/img/img-hp-02-2x.jpg') . ' 2x' ?>">
-          <?= theme('image', array(
-            'path' => $theme_path . '/img/img-hp-mobile-02.jpg',
-            'alt' => "Produkty vizuál",
-            'attributes' => array('srcset' => $directory . '/img/img-hp-02.jpg 2x'),
-          )) ?>
-        </picture>
-      </div>
-      <h2 class="l-home-nav__heading"><span>Produkty</span></h2>
-    </a>
-  </section>
-
   <?php if ($messages): ?>
     <div id="messages">
       <div class="section clearfix">
@@ -62,11 +21,24 @@ include($theme_path . '/templates/inc/header.php');
     </div>
   <?php endif; ?>
 
-  <section class="l-home-content">
+  <?php if ($page['featured']): ?>
+    <div id="featured" class="container">
+      <div class="section clearfix">
+        <?php print render($page['featured']); ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($tabs): ?><?php print render($tabs); ?><?php endif; ?>
+  <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+
+  <section class="l-home-content l-home-content__v2">
     <div class="container">
       <?php print render($page['content']); ?>
     </div>
   </section>
+
+  <?php print render($page['footer']); ?>
 </main>
 
 <?php include($theme_path . '/templates/inc/footer.php'); ?>
