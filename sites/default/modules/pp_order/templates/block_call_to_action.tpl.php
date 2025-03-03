3e99055875f8f78ee12b7576fd31559e13733b39
Jiří Bažant <j.bazant@gmail.com>
2025-03-03 22:15:10 +0100

    feat: update call to action positioning

--- sites/default/modules/pp_order/templates/block_call_to_action.tpl.php
+++ sites/default/modules/pp_order/templates/block_call_to_action.tpl.php
@@ -6,13 +6,15 @@
  * Time: 21:47
  */
 ?>
-<div class="call-to-action">
-    <p>Zaujala Vás naš nabídka pro <span class="page-title">„<?php print drupal_get_title(); ?>“</span>? Nebo k ní máte dotaz?</p>
+<div class="container">
+  <div class="call-to-action">
+    <p>Zaujala Vás naš nabídka pro <span class="page-title">„<?php print drupal_get_title(); ?>“</span>? Nebo k ní máte
+      dotaz?</p>
     <p>
-        Kontaktujte nás na tel. čísle <a class="p-tel" href="tel:+420731566609">+420 731 566 609</a>,
-        nebo pomocí <?php print l(
-            'poptávkového formuláře',
-            'poptavka'
-        ) ?>.</p>
-    <p></p>
+      Kontaktujte nás na tel. čísle <a class="p-tel" href="tel:+420731566609">+420 731 566 609</a>,
+      nebo pomocí <?php print l(
+        'poptávkového formuláře',
+        'poptavka'
+      ) ?>.</p>
+  </div>
 </div>
